98602ff9732672d75896bda59f9b9570f75f78db
Isaac Lins <isaac.lins07@gmail.com>
2024-09-16 21:06:28 +0200

    refactor: add environment variable for webhook URL in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    WEBHOOKURL: 'aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTI4NTMwNzcyOTAwNjgyNTY3NC93SG83YmlMRlg4ZFloM2NOZUFxQXVyN291aVpEYUN5U003RzFhLU1BM1BmVGFjTEI2Z2I4M0xuQmVsZkNOUFdvZXM3Vg==',
+  },
+};
 
 export default nextConfig;
