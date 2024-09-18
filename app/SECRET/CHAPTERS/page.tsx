92e7021a77638eda17d6fe3a4180ca48cc77b3df
isaaclins <isaac.lins07@gmail.com>
2024-09-18 13:39:59 +0200

    Refactor import order in page.tsx and add chapter links

--- app/SECRET/CHAPTERS/page.tsx
+++ app/SECRET/CHAPTERS/page.tsx
@@ -26,6 +26,13 @@ export default function Chapterssite() {
             </p>
             <hr className="my-4" />
             <a href="CHAPTERS/">CHAPTERS:</a>
+            <ul className="list-disc list-inside">
+            <li><a href="/SECRET/CHAPTERS/1">1. What defines me?</a></li>
+            <li><a href="/SECRET/CHAPTERS/2">2. If you know me, you don&apos;t</a></li>
+            <li><a href="/SECRET/CHAPTERS/3">3. Untitled</a></li>
+            <li><a href="/SECRET/CHAPTERS/4">4. Untitled</a></li>
+            <li><a href="/SECRET/CHAPTERS/5">5. Untitled</a></li>
+          </ul>
           </div>
         </div>
       </div>
