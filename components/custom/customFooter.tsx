18a23f301b61668e5aebad0405984bd9d43b08a2
Isaac Lins <contact@isaaclins.com>
2024-09-18 10:44:22 +0200

    Update customFooter.tsx

--- components/custom/customFooter.tsx
+++ components/custom/customFooter.tsx
@@ -2,19 +2,31 @@ import React from 'react';
 import { ChevronRight, ChevronLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-const CustomFooter: React.FC = () => {
+interface CustomFooterProps {
+    Lastpage: string;
+    Nextpage: string;
+    Feedback: boolean;
+}
+
+const CustomFooter: React.FC<CustomFooterProps> = ({ Lastpage, Nextpage, Feedback }) => {
     return (
         <footer className="footer border p-5 backdrop-blur-sm flex justify-center bottom-10 left-20 right-20 z-10">
             <div className="flex space-x-16">
-                <Button variant="outline" size="icon">
-                    <ChevronLeft className="h-4 w-4" />
-                </Button>
-                <a href="/SECRET/feedback">
-                    <Button variant="outline">Leave Feedback</Button>
+                <a href={Lastpage}>
+                    <Button variant="outline" size="icon">
+                        <ChevronLeft className="h-4 w-4" />
+                    </Button>
                 </a>
-                <Button variant="outline" size="icon">
-                    <ChevronRight className="h-4 w-4" />
-                </Button>
+                {Feedback && (
+                    <a href="/SECRET/feedback">
+                        <Button variant="outline">Leave Feedback</Button>
+                    </a>
+                )}
+                <a href={Nextpage}>
+                    <Button variant="outline" size="icon">
+                        <ChevronRight className="h-4 w-4" />
+                    </Button>
+                </a>
             </div>
         </footer>
     );
