ba4d967ccbcc169a8276880983a13fe458e0ac0b
isaaclins <isaac.lins07@gmail.com>
2024-09-18 11:30:45 +0200

    Refactor breadcrumb components
    - Deleted customBreadCrumb.tsx
    - Added customHeader.tsx
    
    The customBreadCrumb.tsx component was removed as it is no longer needed. Instead, the customHeader.tsx component was added to handle the breadcrumb functionality. This change improves the organization and structure of the codebase.

---
+++ components/custom/customHeader.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Slash } from "lucide-react";
+
+interface CustomHeaderProps {
+  main: boolean;
+  parents: string[];
+  child: string;
+}
+
+const CustomHeader: React.FC<CustomHeaderProps> = ({
+  main,
+  parents,
+  child,
+}) => {
+  const parentPath = parents.join("/");
+
+  return (
+    <header className="header sticky top-10 mt-2 z-10 m-10 border p-5 backdrop-blur-md">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {parents.map((parent, index) => (
+            <React.Fragment key={parent}>
+              <BreadcrumbItem>
+                <BreadcrumbLink
+                  className={`${main ? "font-bold" : "font-thin"} text-white`}
+                  href={`/${parents.slice(0, index + 1).join("/")}`}
+                >
+                  {parent}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              {index < parents.length - 1 && (
+                <BreadcrumbSeparator>
+                  <Slash />
+                </BreadcrumbSeparator>
+              )}
+            </React.Fragment>
+          ))}
+          <Slash />
+          <BreadcrumbItem>
+            <BreadcrumbLink
+              className="font-bold text-white"
+              href={`/${parentPath}/${child}`}
+            >
+              {child}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </header>
+  );
+};
+
+export default CustomHeader;
