6c6d365f3519ecc91e4056de09ddb1e9ca1d409c
Isaac Lins <contact@isaaclins.com>
2024-09-19 13:45:15 +0200

    Update page.tsx to match /SECRET/chapters/x

--- app/CHAPTERS/page.tsx
+++ app/CHAPTERS/page.tsx
@@ -25,13 +25,13 @@ export default function Chapterssite() {
               the most unlikely of places.
             </p>
             <hr className="my-4" />
-            <a href="/CHAPTERS/">CHAPTERS:</a>
+            <a href="CHAPTERS/">CHAPTERS:</a>
             <ul className="list-disc list-inside">
-              <li><a href="/CHAPTERS/1">1. What defines me?</a></li>
-              <li><a href="/CHAPTERS/2">2. If you know me, you don&apos;t</a></li>
-              <li><a href="/CHAPTERS/3">3. Untitled</a></li>
-              <li><a href="/CHAPTERS/4">4. Untitled</a></li>
-              <li><a href="/CHAPTERS/5">5. Untitled</a></li>
+              <li><a href="CHAPTERS/1">1. What defines me?</a></li>
+              <li><a href="CHAPTERS/2">2. If you know me, you don&apos;t</a></li>
+              <li><a href="CHAPTERS/3">3. Untitled</a></li>
+              <li><a href="CHAPTERS/4">4. Untitled</a></li>
+              <li><a href="CHAPTERS/5">5. Untitled</a></li>
             </ul>
           </div>
         </div>
